884ec1864986db6c443c9f82daade9aa33d4a45d
Tim <timzdeveloper23@gmail.com>
2018-05-03 05:57:02 +1200

    fk2

--- js/all.js
+++
@@ -1,6 +0,0 @@
-$(document).on('mouseenter', '[data-toggle="tab"]', function () {
-    $(this).tab('show');
-});
-$(document).on('click', '[data-toggle="tab"]', function () {
-    window.location.href=$(this).attr("data-url");
-});
